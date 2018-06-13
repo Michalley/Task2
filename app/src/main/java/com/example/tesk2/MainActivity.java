6b75973ec11c9f65b64f90af18dab9bcc7a7ec2a
michalley <ml5877@bs.amalnet.k12.il>
2018-06-13 13:09:35 +0300

    1st commit

---
+++ app/src/main/java/com/example/tesk2/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.tesk2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    int clickCounter=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+    }
+
+    public void click(View view) {
+        clickCounter = clickCounter+1;
+        btn.setText("This is a click number: "+clickCounter);
+    }
+}
